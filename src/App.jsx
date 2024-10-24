792c55a5d5c45eebc17d3555a12be61e6f8da4dc
MakeArtKenji <Kennonirom@gmail.com>
2024-10-24 21:09:29 +0800

    pomodorofiy front #1

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+// App.js
+import './App.css';
+import FooterSection from './components/FooterSections';
+import NavbarSection from './components/NavbarSection';
+import TasksSection from './components/TaskSection';
+import TimerSection from './components/TimerSection';
+import { useDispatch, useSelector } from 'react-redux';
+import { setActiveTab } from './redux/appSlice'; // Import the action
+
+function App() {
+    const dispatch = useDispatch();
+    const activeTab = useSelector((state) => state.app.activeTab); // Get activeTab from Redux
+
+    // Determine background class based on activeTab
+    const backgroundClass = 
+        activeTab === 'Long Break' ? 'bg-long-break' :
+        activeTab === 'Short Break' ? 'bg-short-break' :
+        'bg-pomodoro';
+
+    // Function to update activeTab in Redux
+    const handleTabChange = (tab) => {
+        dispatch(setActiveTab(tab)); // Dispatch the action to update Redux state
+    };
+
+    return (
+        <div className={`${backgroundClass} min-h-screen flex flex-col`}>
+            <div className="max-w-2xl mx-auto w-full flex-grow items-center">
+                <NavbarSection />
+                <TimerSection activeTab={activeTab} setActiveTab={handleTabChange} /> {/* Pass the Redux state and handler */}
+                <TasksSection />
+            </div>
+            <FooterSection />
+        </div>
+    );
+}
+
+export default App;
